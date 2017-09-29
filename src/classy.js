35afc2de5b31414a95ed97bb6fd0125584c1736b
Yutaka Houlette <yutakahoulette@gmail.com>
2017-09-28 20:53:43 -0700

    Successfully get Classy supporter info

--- src/classy.js
+++ src/classy.js
@@ -1,8 +1,9 @@
 const request = require('request')
 const baseUrl = 'https://api.classy.org/'
+const orgId = '27460'
 
 function getToken (cb) {
-  return request({
+  request({
     method: 'POST',
     baseUrl,
     url: '/oauth2/auth',
@@ -17,9 +18,22 @@ function getToken (cb) {
   })
 }
 
+function getSupporters (token) {
+  request({
+    method: 'GET',
+    baseUrl,
+    url: `2.0/organizations/${orgId}/supporters`,
+    auth: {bearer: token}
+  }, function (err, resp) {
+    if (err) return console.log(err)
+    console.log(JSON.parse(resp.body))
+  })
+}
+
+// todo: use run-waterfall to better organize callbacks
 function init () {
   getToken(function (token) {
-    console.log(token)
+    getSupporters(token)
   })
 }
 
