7b74457c9d076a4111142cb94ca3512194c1fba3
Yutaka Houlette <yutakahoulette@gmail.com>
2017-09-29 12:07:11 -0700

    Successfully get all transation pages

--- src/classy.js
+++ src/classy.js
@@ -1,5 +1,6 @@
 const R = require('ramda')
 const request = require('request')
+const baseUrl = 'https://api.classy.org'
 
 const getDateTime = () => new Date().toUTCString()
 
@@ -13,11 +14,11 @@ const  handleErr = err => {
 // a wrapper for request that sets defaults and handles err and resp
 function req(options, callback, successMsgAction){
   request(R.merge({
-    baseUrl: 'https://api.classy.org/',
+    baseUrl,
     json: true
   }, options), function(err, resp){
     if(err) return handleErr(err)
-    log(`✅  Successfully ${successMsgAction}.`)
+    log(`✅  Successfully ${successMsgAction}`)
     callback(resp)
   })
 }
@@ -34,27 +35,49 @@ function getToken (cb) {
   }, cb, 'requested access token.')
 }
 
-function getSupporters (resp) {
+const getNextPage = (token) => (resp) => {
+  const body = resp.body
+  console.log(body)
+  const nextPageUrl = body.next_page_url
+  if (nextPageUrl) {
+    getTransations({
+      url: nextPageUrl.split(baseUrl)[1],
+      cb: getNextPage(token),
+      msg: 'retrieving page.',
+      token
+    })
+  } else {
+  }
+}
+
+function getTransations ({url, cb, msg, token}) {
   req({
     method: 'GET',
-    url: `2.0/organizations/${process.env.CLASSY_ORG_ID}/transactions`,
+    url,
     qs: {
       'with': 'supporter',
-      fields: 'member_name,supporter'
+      fields: 'member_name,supporter',
+      per_page: 100
     },
-    auth: {bearer: resp.body.access_token}
-  }, function (resp) {
-  }, 'retrieved supporter data.')
+    auth: {bearer: token}
+  }, cb, msg)
 }
 
-function formatData (resp) {
-
+function getAllTransactions (resp) {
+  const token = resp.body.access_token
+  getTransations({
+    url: `2.0/organizations/${process.env.CLASSY_ORG_ID}/transactions`,
+    cb: getNextPage(token),
+    msg: 'retrieving page.',
+    token
+  })
 }
+
 
 // todo: use run-waterfall to better organize callbacks
 function init () {
   log('🤖  connecting to Classy API...')
-  getToken(getSupporters)
+  getToken(getAllTransactions)
 }
 
 module.exports = init()
